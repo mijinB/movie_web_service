c56d6a6b3287d20e2e4a1e6dd7857bae48a45613
mijinB <bmj13465@gmail.com>
2023-08-16 18:28:12 +0900

    @code_challenge USD입력 후 환율@

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const exchangeRateResult = document.getElementById("exchange-rate-result");
+
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myUSD, setMyUSD] = useState([]);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -12,18 +15,44 @@ function App() {
         setLoading(false);
       });
   }, []);
+
+  const onCoinChange = (event) => {
+    setMyUSD(event.target.value);
+  }
+
+  const onExchangeRate = (event) => {
+    exchangeRateResult.innerHTML = `<strong>result👉</strong> ${myUSD / event.target.value}💲`;
+  }
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading
         ? <strong>Loading...</strong>
-        : <ul>
-          {coins.map(coin => (
-            <li>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </li>
-          ))}
-        </ul>
+        : <>
+          <input
+            value={myUSD}
+            type="number"
+            placeholder="Please Write your USD"
+            onChange={onCoinChange}
+          />
+          <strong> USD</strong>
+          <br />
+          <select
+            onChange={onExchangeRate}
+            style={{ marginTop: 10, marginBottom: 10 }}
+          >
+            {coins.map(coin => (
+              <option
+                key={coin.id}
+                value={coin.quotes.USD.price}
+              >
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div id="exchange-rate-result"></div>
+        </>
       }
     </div>
   );
