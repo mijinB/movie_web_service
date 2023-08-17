ff977e1954b0f8e1a4d6438afea74f26b1b419e2
mijinB <bmj13465@gmail.com>
2023-08-17 16:06:13 +0900

    router에서 받아온 값을 useParams로 사용하기

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/movie" element={<Detail />} />
+      <Route path="/movie/:id" element={<Detail />} />
     </Routes>
   );
 }
