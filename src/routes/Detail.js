1e86ab5dae987dc3bd7430395617c0558eefbf74
mijinB <bmj13465@gmail.com>
2023-08-17 16:14:22 +0900

    @code_challenge Detail페이지에 Home페이지 처럼 data 표출하기@

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
 
   const getMovie = async () => {
     const json = await (
@@ -10,13 +14,29 @@ function Detail() {
         `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
       )
     ).json();
-    console.log(json);
+
+    setMovie(json.data.movie);
+    setLoading(false);
   }
 
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1>Detail</h1>
+  return (
+    <div>
+      {loading
+        ? <h1>Loading...</h1>
+        : <Movie
+          key={movie.id}
+          id={movie.id}
+          coverImg={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.summary}
+          genres={movie.genres}
+        />
+      }
+    </div>
+  );
 }
 export default Detail;
